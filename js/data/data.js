626f98e64aa921a65841589be43c5f97ae1a2fdf
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-20 11:10:06 +0200

    initial data

--- js/data/data.js
+++ js/data/data.js
@@ -1,4 +1,44 @@
 const data = {
+    tasks:[
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia butu slapia ir silta',
+            status:'todo',
+        }, 
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia butu slapia ir silta',
+            status:'in-progress',
+        }, 
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia butu slapia ir silta',
+            status:'in-progres',
+        }, 
+        {
+            title: 'Reikia pasidaryti arbatos',
+            description: 'Reikia butu slapia ir silta',
+            status:'todo',
+        }, 
+    ],
+    columns:[
+        {
+            title: 'Task ready',
+            status: 'todo',
+        }, 
+        {
+            title: 'In progress',
+            status: 'in progress',
+        },
+        {
+            title: 'Needs review',
+            status: 'review',
+        },
+        {
+            title: 'Done',
+            status: 'done',
+        }
+    ]
     
 };
 
